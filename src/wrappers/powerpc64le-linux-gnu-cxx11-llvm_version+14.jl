454e0f781ba26eae40867254904bd3629e3e187d
jlbuild <juliabuildbot@gmail.com>
2024-10-16 08:52:21 +0000

    pocl_jll build 6.0.0+5

--- src/wrappers/powerpc64le-linux-gnu-cxx11-llvm_version+14.jl
+++ src/wrappers/powerpc64le-linux-gnu-cxx11-llvm_version+14.jl
@@ -6,11 +6,12 @@ using Hwloc_jll
 using SPIRV_LLVM_Translator_unified_jll
 using SPIRV_Tools_jll
 using Clang_unified_jll
+using LLD_unified_jll
 JLLWrappers.@generate_wrapper_header("pocl")
 JLLWrappers.@declare_library_product(libpocl, "libpocl.so.2")
 JLLWrappers.@declare_executable_product(poclcc)
 function __init__()
-    JLLWrappers.@generate_init_header(OpenCL_jll, Hwloc_jll, SPIRV_LLVM_Translator_unified_jll, SPIRV_Tools_jll, Clang_unified_jll)
+    JLLWrappers.@generate_init_header(OpenCL_jll, Hwloc_jll, SPIRV_LLVM_Translator_unified_jll, SPIRV_Tools_jll, Clang_unified_jll, LLD_unified_jll)
     JLLWrappers.@init_library_product(
         libpocl,
         "lib/libpocl.so",
@@ -97,5 +98,17 @@ function __init__()
                                 SPIRV_LLVM_Translator_unified_jll.llvm_spirv_path,
                                 SPIRV_LLVM_Translator_unified_jll.LIBPATH[],
                                 SPIRV_LLVM_Translator_unified_jll.PATH[])
+    ld_path = if Sys.islinux()
+            LLD_unified_jll.ld_lld_path
+        elseif Sys.isapple()
+            LLD_unified_jll.ld64_lld_path
+        elseif Sys.iswindows()
+            LLD_unified_jll.lld_link_path
+        else
+            error("Unsupported platform")
+        end
+    ENV["POCL_PATH_LD"] =
+        generate_wrapper_script("lld", ld_path,
+                                LLD_unified_jll.LIBPATH[], LLD_unified_jll.PATH[])
 
 end  # __init__()
